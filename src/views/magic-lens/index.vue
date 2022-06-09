28681bdd13ef6ed881897bf97fc88a401faf5768
QuyenTT <quyen.tranthe97@gmail.com>
2022-06-09 20:20:03 +0700

    acccc

--- src/views/magic-lens/index.vue
+++ src/views/magic-lens/index.vue
@@ -15,23 +15,25 @@
             v-for="(item, index) in dataList"
             :key="index"
             class="flex justify-center flex-col items-center cursor-pointer mr-5"
-            @click="showProfile(item.fbid)"
+            @click="showProfile(item.fbid, index)"
           >
             <img class="w-30 h-30 rounded-lg" :src="item.imgPath" alt="">
             <p class="p-2 mt-3 text-center text-white rounded bg-magic-level1 text-xs w-24">{{
-              item.userStatus === 'vip' ? 'VIP' : item.userStatus === 'new' ? 'NEW' : item.userStatus === 'normal' ? 'NORMAL' : ''
+              item.userStatus.toUpperCase()
             }}</p>
           </div>
         </div>
       </div>
-      <div class="py-6 container mx-auto mt-8 px-11 rounded border border-solid border-magic-borderMagic">
+      <div 
+        v-show="dataDetail" class="py-6 container mx-auto mt-8 px-11 rounded border border-solid border-magic-borderMagic">
         <h2 class="text-xl font-bold mb-6 text-VNDG-listMagicLen">Chi tiết người vào</h2>
         <div class="grid grid-cols-16 gap-x-8">
           <div class="rounded shadow-md bg-white p-6">
             <div class="flex justify-center items-center flex-col">
-              <img class="w-30 h-30 rounded-lg" src="@/assets/default.svg" alt="">
-              <p class="text-xl font-bold text-center mb-5 mt-4 text-VNDG-listMagicLen">Nguyễn Thị Tú Uyên</p>
-              <div class="w-24 text-xs text-white bg-magic-level1 p-2 rounded">Người cao cấp</div>
+              <img v-if="getData(dataDetail.imgPath) == ''" class="w-30 h-30 rounded-lg" src="@/assets/default.svg" alt="">
+              <img v-else class="w-30 h-30 rounded-lg" :src="getData(dataDetail.imgPath)" alt="">
+              <p class="text-xl font-bold text-center mb-5 mt-4 text-VNDG-listMagicLen">{{ getData(dataDetail.name)}}</p>
+              <div class="w-24 text-xs text-white text-center bg-magic-level1 p-2 rounded">{{ getData(dataDetail.userStatus).toUpperCase()}}</div>
             </div>
           </div>
           <div class="shadow grid grid-cols-2 pt-7 pl-10 pr-6 gap-6 pb-16 relative">
@@ -50,35 +52,40 @@
                   <img class="mr-2" src="@/assets/user.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Họ tên</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">Nguyễn Thị Tú Uyên</p>
+                <p class="text-sm text-magic-listMagicLen">{{ getData(dataDetail.name)}}</p>
               </div>
               <div class="grid grid-cols-2 items-center mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/calendar.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Ngày sinh</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">11/11/2001</p>
+                <p class="text-sm text-magic-listMagicLen">{{ getData(dataDetail.dob)}}</p>
               </div>
               <div class="grid grid-cols-2 items-center mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/pen.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Số chứng minh nhân dân</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">00008888</p>
+                <p class="text-sm text-magic-listMagicLen">{{ getData(dataDetail.idNum)}}</p>
               </div>
               <div class="grid grid-cols-2 items-center mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/phone.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Số điện thoại</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">00008888</p>
+                <p class="text-sm text-magic-listMagicLen">{{ getData(dataDetail.phoneNum)}}</p>
               </div>
               <div class="grid grid-cols-2 items-center mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/facebook.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Facebook</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen underline">Link</p>
+                <a :href="getLinkFb(dataDetail.fbid)"
+                target="_blank">
+                  <p class="text-sm text-magic-listMagicLen underline" style="color: blue">
+                    {{  getLinkFb(dataDetail.fbid) == '' ? '' : 'Facebook'}}
+                    </p>
+                </a>
               </div>
             </div>
             <div class="flex flex-col">
@@ -87,14 +94,14 @@
                   <img class="mr-2" src="@/assets/location.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Quê quán</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">Quận Cầu Giấy, thành phố Hà Nội</p>
+                <p class="text-sm text-magic-listMagicLen">{{ getData(dataDetail.address)}}</p>
               </div>
               <div class="grid grid-cols-2 items-center mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/pen.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Số tài khoản</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">012345666999</p>
+                <p class="text-sm text-magic-listMagicLen">{{ getData(dataDetail.stk)}}</p>
               </div>
               <div class="grid grid-cols-2 items-center mb-5">
                 <p class="flex">
@@ -108,36 +115,38 @@
                   <img class="mr-2" src="@/assets/copy.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Trạng thái</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">Người cao cấp</p>
+                <p class="text-sm text-magic-listMagicLen">{{ getData(dataDetail.userStatus).toUpperCase()}}</p>
               </div>
             </div>
-            <div v-show="showDetail" class="flex flex-col">
+            <div v-show="showDetail">
               <div class="grid grid-cols-150 items-start mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/location.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Địa chỉ tạm trú</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">Quận Cầu Giấy, thành phố Hà Nội</p>
+                <p class="text-sm text-magic-listMagicLen">
+                  {{ getAddress(dataFb.address) }}
+                  <!-- <div class="float-right" v-loading="isLoading"
+                    element-loading-spinner="el-icon-loading"
+                    element-loading-background="rgba(0, 0, 0, 0)"/> -->
+                </p>
               </div>
               <div class="grid grid-cols-150 items-start mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/user2.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Thông tin người thân</span>
                 </p>
-                <ul class="list-disc">
-                  <li class="text-sm text-magic-listMagicLen">Bố ruột: Nguyễn Thành Nam (sdt 0171973917,<span
-                    class="underline"
-                  >Facebook</span> )
-                  </li>
-                  <li class="text-sm text-magic-listMagicLen">Mẹ ruột: Nguyễn Thành Nam (sdt 0171973917, <span
-                    class="underline"
-                  >Facebook</span>)
-                  </li>
-                  <li class="text-sm text-magic-listMagicLen">Em ruột: Nguyễn Thành Nam (sdt 0171973917, <span
-                    class="underline"
-                  >Facebook</span>)
-                  </li>
-                </ul>
+                <div v-if="dataFb.homie && dataFb.homie.length" class="list-disc">
+                  <div v-for="item in dataFb.homie" :key="item">
+                    <p class="text-sm text-magic-listMagicLen">
+                      {{ item.name + '(' + item.phone + ", "}}
+                      <a class="underline" style="color: blue" :href="getLinkFb2(item.facebook_link)" target="_blank">
+                        {{getData(item.facebook_link) == '' ? '' : 'Facebook'}}
+                      </a>
+                      )
+                    </p>
+                  </div>
+                </div>
                 <!-- <p class="text-sm text-magic-listMagicLen">Ổn định</p> -->
               </div>
               <div class="grid grid-cols-150 items-start mb-5">
@@ -145,7 +154,7 @@
                   <img class="mr-2" src="@/assets/job.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Thông tin công việc</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">Ổn định</p>
+                <p class="text-sm text-magic-listMagicLen">{{dataFb.job_status}}</p>
               </div>
             </div>
             <div v-show="showDetail" class="flex flex-col">
@@ -154,29 +163,31 @@
                   <img class="mr-2" src="@/assets/location.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Địa làm việc</span>
                 </p>
-                <p class="text-sm text-magic-listMagicLen">Phường Quan Hoa, Quận Cầu Giấy, thành phố Hà Nội</p>
+                <p class="text-sm text-magic-listMagicLen">
+                  {{ getAddress(dataFb.work_address) }}
+                </p>
               </div>
               <div class="grid grid-cols-150 items-start mb-5">
                 <p class="flex">
                   <img class="mr-2" src="@/assets/user2.svg" alt="">
                   <span class="text-sm font-normal text-magic-level4">Thông tin đồng nghiệp:</span>
                 </p>
-                <ul class="list-disc">
-                  <li class="text-sm text-magic-listMagicLen">Sếp: Nguyễn Thành Nam (sdt 0171973917, <span
-                    class="underline"
-                  >Facebook</span> )
-                  </li>
-                  <li class="text-sm text-magic-listMagicLen">Cấp dưới: Nguyễn Thành Nam (sdt 0171973917, <span
-                    class="underline"
-                  >Facebook</span>)
-                  </li>
-                </ul>
+                <div v-if="dataFb.co-worker && dataFb.co-worker.length">
+                  <div v-for="item in dataFb.co-worker" :key="item">
+                    <p class="text-sm text-magic-listMagicLen">
+                      {{ item.name + '(' + item.phone + ", "}}
+                      <a class="underline" style="color: blue" :href="getLinkFb2(item.facebook_link)" target="_blank">
+                        {{getData(item.facebook_link) == '' ? '' : 'Facebook'}}
+                      </a>
+                      )
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -187,7 +198,10 @@ export default {
   data() {
     return {
       showDetail: false,
-      dataList: []
+      dataList: [],
+      dataDetail: '',
+      dataFb: '',
+      isLoading: false
     }
   },
   mounted() {
@@ -198,13 +212,49 @@ export default {
   methods: {
     async getListData() {
       const res = await axios.get('https://demo.vndcredit.vn/server/api/queue')
-      console.log('res', res)
       this.dataList = res.data.Data.data
     },
-    async showProfile(id) {
-      id = '100005227800848'
-      const res = await axios.get('https://demo.vndcredit.vn/data/?fb_id=' + `${id}` + '&phone=')
-      console.log(res)
+    showProfile(id, index) {
+      this.dataDetail = this.dataList[index]
+      if (id && id != '') {
+        this.isLoading = true
+        axios.get('https://demo.vndcredit.vn/data/?fb_id=' + `${id}` + '&phone=').then(res => {
+          this.dataFb =  res.data
+          this.isLoading = false
+        }) 
+      } else {
+        this.dataFb = ''
+      }
+    },
+    getData(data) {
+      if (data) {
+        return data
+      } else {
+        return ''
+      }
+    },
+    getLinkFb(data) {
+      if (data) {
+        return 'https://fb.com/' + data
+      } else {
+        return ''
+      }
+    },
+    getLinkFb2(data) {
+      if (data) {
+        return 'https://' + data
+      } else {
+        return ''
+      }
+    },
+    getAddress(data) {
+      var result = ''
+      if (data && data.length) {
+        data.forEach(element => {
+          result = result + " "
+        })
+      }
+      return result
     }
   }
 }
