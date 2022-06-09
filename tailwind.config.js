8e595a777b801359e76339fb7a3bc9fcf8757dc0
Phạm Ngọc Nhật <nhatpn@vndgroup.vn>
2022-06-09 15:38:43 +0700

    fix bugs

---
+++ tailwind.config.js
@@ -0,0 +1,93 @@
+const {
+  theme: { fontSize, lineHeight, spacing }
+} = require('tailwindcss/defaultConfig')
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    spacing: {
+      1.5: '0.375rem',
+      2.5: '0.625rem',
+      4.5: '1.125rem',
+      18: '4.5rem',
+      30: '7.5rem',
+      ...spacing
+    },
+    truncate: {
+      lines: {
+        2: '2',
+        3: '3',
+        4: '4'
+      }
+    },
+    fontSize: {
+      '2.5xl': '1.75rem',
+      '3.5xl': '2rem',
+      '4.5xl': '2.5rem',
+      '6.5xl': '4rem',
+      ...fontSize
+    },
+    lineHeight: {
+      3.5: '0.875rem',
+      5.5: '1.375rem',
+      6.5: '1.5625rem',
+      7.5: '1.875rem',
+      12: '3rem',
+      12.5: '3.125rem',
+      slideSm: '60px',
+      slideXl: '140px',
+      ...lineHeight
+    },
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '16': '348px 1fr',
+        '150': '190px 1fr'
+      },
+      colors: {
+        magic: {
+          borderMagic: '#E6EBF4',
+          listMagicLen: '#292C53',
+          level1: '#FFC207',
+          level2: '#5F69E7',
+          level3: '#C1C5D7',
+          level4: '#868A9E'
+        },
+        VNDG: {
+          borderMagic: '#E6EBF4',
+          listMagicLen: '#292C53',
+          buttonBlue: '#0066CC',
+          stateBlue: '#3385D6',
+          yellow: '#3385D6',
+          red: '#F7685B',
+          green: '#2ED47A',
+          dRed: '#E42E2E',
+          gray600: '#718096',
+          gray700: '#4A5568',
+          tableGray: '#707683',
+          mainDblue: '#213A52',
+          newGray: '#A3A6B1',
+          disableGray: '#E5E5E5',
+          darkBlue: '#334D6E',
+          gray: '#90A0B7',
+          iconGray: '#C2CFE0',
+          grayCyan: '#EDF2F7',
+          lightGrayCyan: '#F7FAFC',
+          gCyan: '#4FD1C5'
+        }
+      }
+    }
+  },
+  variants: {
+    margin: ['responsive', 'first', 'last', 'hover', 'focus'],
+    overflow: ['responsive', 'hover'],
+    padding: ['responsive', 'first', 'last', 'hover', 'focus'],
+    borderRadius: ['responsive', 'hover', 'focus'],
+    extend: {}
+  },
+  plugins: [require('tailwindcss-truncate-multiline')(['responsive', 'hover'])],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production'
+    // content: ['components/**/*.vue', 'views/**/*.vue', 'plugins/**/*.js', 'vue.config.js']
+  }
+}
